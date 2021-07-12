2c0597fedcb2671e5361ad4feebdf9395af07901
esdandreu <esdandreu@gmail.com>
2021-07-12 17:43:02 +0200

    CU-n0w8u1 profile model

--- integration/models.py
+++ integration/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # TODO Validate always starts with pk
+    clickup_pk = models.CharField(blank=True)
+    google_auth_token = models.CharField(blank=True, editable=False)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()

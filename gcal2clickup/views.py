0e5d7fd070abc5480bab3bb68f26181a46c468ac
esdandreu <esdandreu@gmail.com>
2021-07-21 10:54:03 +0200

    still testing

--- gcal2clickup/views.py
+++ gcal2clickup/views.py
@@ -4,7 +4,11 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def google_calendar_endpoint(request):
+    # TODO If it is a sync message save the webhook data
+    # TODO If it is a notification, check the resource that was changed
     print(request)
     print(request.body)
     print(request.POST)
+    print(request.META)
+    print(request.headers)
     return HttpResponse('Hello wolrd')

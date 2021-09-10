00a28f28ec4fa168d86e1e9d82980e5ac50be5da
esdandreu <esdandreu@gmail.com>
2021-09-10 17:36:34 +0200

    improved responses

--- gcal2clickup/views.py
+++ gcal2clickup/views.py
@@ -9,6 +9,7 @@ import json
 
 logger = logging.getLogger('gcal2clikup')
 
+
 @csrf_exempt
 def google_calendar_endpoint(request):
     state = request.headers.get('X-Goog-Resource-State', None)
@@ -24,9 +25,11 @@ def google_calendar_endpoint(request):
         # Ignore signals from inactive users
         if not webhook.user.is_active:
             logger.info('Ignoring google calendar change')
-            return HttpResponse('Ignored', status=200)
-        webhook.check_events()
-        return HttpResponse(status=200)
+            return HttpResponse('Ignored', status=218)
+        created, updated = webhook.check_events()
+        return HttpResponse(
+            f'Created {created} tasks, updated {updated} tasks', status=200
+            )
     return HttpResponseForbidden()
 
 
@@ -40,7 +43,7 @@ def clickup_endpoint(request):
     # Ignore signals from inactive users
     if not webhook.clickup_user.user.is_active:
         logger.info(body)
-        return HttpResponse('Ignored', status=200)
+        return HttpResponse('Ignored', status=218)
     task_id = body['task_id']
     event = body['event']
     items = body.get('history_items', [])
@@ -49,17 +52,21 @@ def clickup_endpoint(request):
             synced_event = SyncedEvent.objects.get(task_id=task_id)
             if event == 'taskDeleted':
                 synced_event.delete(with_event=True)
+                return HttpResponse('Event deleted', status=204)
             else:
                 synced_event.update_event_from_task_history(items)
                 synced_event.save()
+                return HttpResponse('Updated event', status=201)
         except SyncedEvent.DoesNotExist:
             # Was sync tag added?
             if event == 'taskUpdated':
                 if ClickupWebhook.is_sync_tag_added(items):
-                    webhook.check_task(task_id=task_id)
+                    if webhook.check_task(task_id=task_id):
+                        return HttpResponse('Created event', status=201)
                 else:
                     webhook.clickup_user.remove_sync_tag(task_id=task_id)
+                    return HttpResponse('Sync removed', status=204)
     except Exception as e:
         logger.error(body)
         raise e
-    return HttpResponse(status=200)
+    return HttpResponse('Nothing happened', status=200)
